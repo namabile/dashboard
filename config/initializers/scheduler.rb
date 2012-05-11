32bf772d7c8e743a9311fa71ebe61c0140c8ee27
Nick Amabile <amabile.n@gmail.com>
2012-05-10 17:23:41 -0700

    add scheduler for visits update

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -10,3 +10,7 @@ end
 scheduler.cron '0 0 * * *', :allow_overlapping => false, :tags => "refresh orders"  do
 	DBTasks.refresh_orders
 end
+
+scheduler.cron '30 0 * * *', :allow_overlapping => false, :tags => "update visits"  do
+	GoogleAnalytics.update_visits
+end
