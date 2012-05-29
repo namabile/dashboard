5fee225a39c7de5ddca890c607b167ed0e15c21a
Nick Amabile <amabile.n@gmail.com>
2012-05-29 10:16:00 -0700

    only activate scheduler in production

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,20 +1,23 @@
-require "rubygems"
-require "rufus/scheduler"
+if Rails.env.production?
 
-# scheduler = Rufus::Scheduler.start_new
+	require "rubygems"
+	require "rufus/scheduler"
 
-# scheduler.every '1m', :allow_overlapping => false, :blocking => true, :tags => "update orders" do
-# 	DBTasks.update_orders
-# end
+	scheduler = Rufus::Scheduler.start_new
 
-# scheduler.cron '0 0 * * *', :allow_overlapping => false, :blocking => true, :tags => "refresh orders"  do
-# 	DBTasks.refresh_orders
-# end
+	scheduler.every '1m', :allow_overlapping => false, :blocking => true, :tags => "update orders" do
+		DBTasks.update_orders
+	end
 
-# scheduler.cron '15 0 * * *', :allow_overlapping => false, :blocking => true, :tags => "update visits"  do
-# 	GoogleAnalytics.update_visits
-# end
+	scheduler.cron '0 0 * * *', :allow_overlapping => false, :blocking => true, :tags => "refresh orders"  do
+		DBTasks.refresh_orders
+	end
 
-# scheduler.cron '30 0 * * *', :allow_overlapping => false, :blocking => true, :tags => "update last year's orders"  do
-# 	DBTasks.update_last_years_orders
-# end
+	scheduler.cron '15 0 * * *', :allow_overlapping => false, :blocking => true, :tags => "update visits"  do
+		GoogleAnalytics.update_visits
+	end
+
+	scheduler.cron '30 0 * * *', :allow_overlapping => false, :blocking => true, :tags => "update last year's orders"  do
+		DBTasks.update_last_years_orders
+	end
+end
